784eab8a560c506348fd5e900b5b816c9911688f
Ohle Claussen <joclaussen@gmail.com>
2018-10-17 15:50:23 +0200

    Install a delegating GlassPane and a KeyEventDispatcher on the focused window

---
+++ sving-core/src/main/java/de/eudaemon/sving/core/SvingGlassPane.java
@@ -0,0 +1,24 @@
+package de.eudaemon.sving.core;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SvingGlassPane extends JComponent {
+    private final Component original;
+    private final Container container;
+
+    public SvingGlassPane(RootPaneContainer container_) {
+        original = container_.getGlassPane();
+        container = (Container) container_;
+        setFocusable(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        original.paint(g);
+    }
+
+    Component getOriginal() {
+        return original;
+    }
+}
--- sving-core/src/main/java/de/eudaemon/sving/core/WindowManager.java
+++ sving-core/src/main/java/de/eudaemon/sving/core/WindowManager.java
@@ -1,13 +1,18 @@
 package de.eudaemon.sving.core;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class WindowManager {
+public class  WindowManager {
 
-    private Window currentlyFocusedWindow;
-    private static final Logger log = Logger.getLogger(WindowManager.class.getName());
+    private RootPaneContainer currentlyFocusedWindow = null;
+    private SvingGlassPane installedGlassPane = null;
+
+    private static final Logger LOG = Logger.getLogger(WindowManager.class.getName());
 
     public WindowManager() {
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addPropertyChangeListener(
@@ -18,11 +23,44 @@ public class WindowManager {
                 "managingFocus",
                 e -> updateWindow()
                 );
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent e) {
+                if (isHotkey(e) && e.getID() == KeyEvent.KEY_RELEASED) {
+                    System.out.println("WindowManager.dispatchKeyEvent");
+                    return true;
+                }
+                return false;
+            }
+
+            private boolean isHotkey(KeyEvent e) {
+                return ((e.getModifiersEx() & InputEvent.CTRL_DOWN_MASK) != 0) && e.getKeyChar() == ';';
+            }
+        });
         updateWindow();
     }
 
     private void updateWindow() {
-        currentlyFocusedWindow = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow();
-        log.log(Level.FINE, "Focus switched to " + (currentlyFocusedWindow == null ? "None" : currentlyFocusedWindow.getName()));
+        RootPaneContainer oldWindow = currentlyFocusedWindow;
+        Window focusedWindow = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow();
+        if (focusedWindow instanceof RootPaneContainer) {
+            currentlyFocusedWindow = (RootPaneContainer) focusedWindow;
+        }
+        LOG.log(Level.FINE, "Focus switched to " + (focusedWindow == null ? "None" : focusedWindow.getName()));
+        if (currentlyFocusedWindow != null && !currentlyFocusedWindow.equals(oldWindow)) {
+            installGlassPane(oldWindow, currentlyFocusedWindow);
+        }
+    }
+
+    private void installGlassPane(
+            RootPaneContainer oldWindow,
+            RootPaneContainer newWindow
+            ) {
+        if (oldWindow != null) {
+            oldWindow.setGlassPane(installedGlassPane.getOriginal());
+        }
+        installedGlassPane = new SvingGlassPane(currentlyFocusedWindow);
+        newWindow.setGlassPane(installedGlassPane);
+        LOG.fine("Installed GlassPane on " + newWindow);
     }
 }
