74ce36a87bc2a3138768c693192d22824f735565
Ohle Claussen <joclaussen@gmail.com>
2018-10-20 23:50:13 +0200

    Remove uneccesary field

--- sving-core/src/main/java/de/eudaemon/sving/core/SvingGlassPane.java
+++ sving-core/src/main/java/de/eudaemon/sving/core/SvingGlassPane.java
@@ -5,11 +5,9 @@ import java.awt.*;
 
 public class SvingGlassPane extends JComponent {
     private final Component original;
-    private final Container container;
 
     public SvingGlassPane(RootPaneContainer container_) {
         original = container_.getGlassPane();
-        container = (Container) container_;
         setFocusable(true);
     }
 
