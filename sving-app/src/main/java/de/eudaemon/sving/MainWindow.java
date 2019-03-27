c2dd79ebc124f1ebe1a697000f92ce750a0e8edf
Ohle Claussen <joclaussen@gmail.com>
2019-03-27 21:54:22 +0100

    Add title and scrollpane

--- sving-app/src/main/java/de/eudaemon/sving/MainWindow.java
+++ sving-app/src/main/java/de/eudaemon/sving/MainWindow.java
@@ -6,10 +6,29 @@ import java.awt.*;
 class MainWindow
         extends JFrame {
 
+    private static final Dimension MINIMUM_WINDOW_SIZE = new Dimension(600, 400);
+    private static final Dimension INFINITE_SIZE = new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
     MainWindow() {
         setLayout(new BorderLayout());
+        setMinimumSize(MINIMUM_WINDOW_SIZE);
+        add(createListPanel(), BorderLayout.CENTER);
+    }
+
+    private JPanel createListPanel() {
+        JPanel listPanel = new JPanel();
+        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.PAGE_AXIS));
+        JLabel header = new JLabel("Running JVMs:");
+        header.setAlignmentX(Component.LEFT_ALIGNMENT);
+        header.setPreferredSize(new Dimension(header.getWidth(), header.getHeight() + 30));
+        listPanel.add(header);
+
         JList<VMListModel.VM> vmList = new JList<>(new VMListModel());
         vmList.setCellRenderer(new DefaultListCellRenderer());
-        add(vmList, BorderLayout.CENTER);
+        vmList.setMaximumSize(INFINITE_SIZE);
+        JScrollPane scrollPane= new JScrollPane(vmList);
+        scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+        listPanel.add(scrollPane);
+        return listPanel;
     }
 }
